d6427a81b7f982fab4ea4408de4e311c32d75c1f
fatihatuzjohora <fatihatuzjohora@gmail.com>
2024-02-19 22:02:35 +0600

    commit

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 let newArray = [];
-const bookingSeat = document.getElementById('booking-seat');
-console.log(bookingSeat.children);
-const allSeat = document.getElementsByClassName("busSitName"); 
+const bookingSeat = document.getElementById("booking-seat");
+const seatCount = document.getElementById('seat-count')
+// console.log(bookingSeat.children);
+const allSeat = document.getElementsByClassName("busSitName");
+const totalSit = document.getElementById('totalSit')
+const totalPrice = document.getElementById('total-price')
 
 for (const seat of allSeat) {
   seat.addEventListener("click", function (e) {
     const seatNameLocation = e.target;
+    seatNameLocation.setAttribute("disabled", "true");
+
     const seatName = e.target.innerText;
     const seatNumberDetails = {
       location: seatNameLocation,
@@ -13,30 +18,51 @@ for (const seat of allSeat) {
       name: seatName,
       className: "Economoy",
     };
-    if (newArray.length <4) {
-  createElement(seatNumberDetails)
-  newArray.push(seatNumberDetails)
-  seatNameLocation.classList.add('bg-green-500')
-  
-}
-console.log(newArray);
+    if (newArray.length < 4) {
+      createElement(seatNumberDetails);
+      newArray.push(seatNumberDetails);
+      totalMoney(newArray)
+      seatNameLocation.classList.add("bg-green-500");
+    }
+    // counting area
+    const countUp = convertNumver(seatCount)+1
+    seatCount.innerText = countUp;
+    const finalSeat = convertNumver(totalSit)-1;
+    totalSit.innerText = finalSeat
   });
 }
 
-function createElement(obj){
-  const div = document.createElement('div')
-const p1 =document.createElement("p")
-p1.innerText =obj.name;
-const p2 =document.createElement("p")
-p2.innerText =obj.className;
+function createElement(obj) {
+  const div = document.createElement("div");
+  const p1 = document.createElement("p");
+  p1.innerText = obj.name;
+  const p2 = document.createElement("p");
+  p2.innerText = obj.className;
 
-const p3 =document.createElement("p")
-p3.innerText =obj.price;
+  const p3 = document.createElement("p");
+  p3.innerText = obj.price;
 
-div.appendChild(p1)
-div.appendChild(p2)
-div.appendChild(p3)
-// console.log(bookingSeat);
+  div.appendChild(p1);
+  div.appendChild(p2);
+  div.appendChild(p3);
+  // console.log(bookingSeat);
+  div.classList.add("flex", "justify-between", "p-2");
 
- bookingSeat.appendChild(div)
+  bookingSeat.appendChild(div);
+}
+
+function convertNumver(id){
+  const number  = parseInt(id.innerText)
+  return number
+}
+
+
+function totalMoney (ary){
+  let sum = 0;
+// console.log(ary);
+for(const seat of ary){
+  console.log(seat.price);
+  sum=sum+seat.price
+}
+totalPrice.innerText = sum
 }
